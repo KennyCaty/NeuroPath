295e604e4f64b96038ade68180afd568f5f2d70e
KennyCaty <leekenny88@outlook.com>
2026-04-28 12:11:56 +0800

    Update example with index_llm/rag_llm args

--- src/run_neuropath_main_exps.sh
+++ src/run_neuropath_main_exps.sh
@@ -2,4 +2,7 @@
 # musique 2wikimultihopqa hotpotqa popqa nq_rear
 
 ## ====Run Example ===
-python3 src/rag_neuropath.py --dataset 2wikimultihopqa --retriever facebook/contriever --top_k 100  --llm_model gpt-4o-mini  --one_shot f --max_hop 2
+python3 src/rag_neuropath.py --dataset 2wikimultihopqa --retriever facebook/contriever --top_k 100 \
+    --index_llm openai --index_llm_model gpt-4o-mini \
+    --rag_llm openai --rag_llm_model gpt-4o-mini \
+    --one_shot f --max_hop 2
