68e79c639075ebb8f6fb1f83941db66dbf4dd45c
KennyCaty <leekenny88@outlook.com>
2026-04-28 12:08:58 +0800

    Drop syn_threshold variable

--- src/setup_neuropath_main_exps.sh
+++ src/setup_neuropath_main_exps.sh
@@ -1,15 +1,13 @@
 gpus_available=0
-syn_threshold=0.8
 
 # The loop here is provided only for illustrative purposes. For actual runs, we recommend running the command separately for each dataset. For example: for data in 2wikimultihopqa;
 # for data in musique 2wikimultihopqa hotpotqa popqa nq_rear multihoprag_chunks;
 # do
-#   # bash src/setup_neuropath.sh $data BAAI/bge-m3 gpt-4o-mini $gpus_available $syn_threshold openai
-#   bash src/setup_neuropath.sh $data facebook/contriever gpt-4o-mini $gpus_available $syn_threshold openai
+#   # bash src/setup_neuropath.sh $data BAAI/bge-m3 gpt-4o-mini $gpus_available openai
+#   bash src/setup_neuropath.sh $data facebook/contriever gpt-4o-mini $gpus_available openai
 # done
 
 # ====== Example ======
 # data=nq_rear
 data=2wikimultihopqa
-bash src/setup_neuropath.sh $data facebook/contriever gpt-4o-mini $gpus_available $syn_threshold openai
-
+bash src/setup_neuropath.sh $data facebook/contriever gpt-4o-mini $gpus_available openai
